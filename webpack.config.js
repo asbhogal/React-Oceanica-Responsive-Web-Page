88d7c09209d2c50db38918473ea895c2b5edd81a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-02-17 16:29:53 +0000

    ran build prod for deployment - additional improvements to UI

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: "images/[hash][ext][query]"
+        assetModuleFilename: "images/[hash][ext][query]",
+        publicPath: '/'
     },
 
     module: {
